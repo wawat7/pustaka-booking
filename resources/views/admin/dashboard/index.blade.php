5e4121858a66a826d30a72eaf5072101b3e3e098
Wawat Prigala <wawatprigala00@gmail.com>
2024-03-15 23:26:31 +0700

    layout admin and import asset front

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin.app')
+
+@section('content')
+<div class="container-fluid">
+
+    DASHBOARD
+
+</div>
+@endsection
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::group(['prefix' => 'auth', 'as' => 'auth.'], function() {
-    Route::get('login', [LoginController::class, 'login'])->name('login');
-});
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
